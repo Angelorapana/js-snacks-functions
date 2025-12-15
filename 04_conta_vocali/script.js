71f70259ee9067641c066364468420cf056df776
angelorapana <angelorapana@icloud.com>
2025-12-15 17:21:21 +0100

    fix contenuto funzione

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+vocali(word);
 
 // Invoca la funzione qui e stampa il risultato in console
 
+ function vocali(word){
+ let ricercaVocali=[];
+ for (let index = 0; index < word.length; index++) {
+    let contenutoVocali=[];
+
+    if (contenutoVocali == "a" && "e" && "i " && "o" && "u"){
+        ricercaVocali.join("");
+    }
+ }return(ricercaVocali);
+ }
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(vocali(word));
