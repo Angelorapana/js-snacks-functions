483784149803622f3e0ae9f4b81eedf0b4314309
angelorapana <angelorapana@icloud.com>
2025-12-15 15:17:30 +0100

    Dichiarazione ed invocazione della funzione

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,16 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
-
+saluto();
 
 // Invoca la funzione qui e stampa il risultato in console
+function saluto(userName){
+    return ("Ciao " + userName);
+}
+
 
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+console.log("Ciao"+ userName )
